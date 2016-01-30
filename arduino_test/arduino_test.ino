0e8dfa8c5ed5a9a2cadc586d6a275a09e129ef82
Lucas Kramer <krame505@umn.edu>
2016-01-30 01:30:58 -0600

    More complex example

--- arduino_test/arduino_test.ino
+++ arduino_test/arduino_test.ino
@@ -16,23 +16,28 @@ String data = "";
 float i = 0;
 float j = 0;
 long last_sent = 0;
+long last_sent1 = 0;
 
 void loop() {
   if (millis() - last_sent > 100) {
     last_sent = millis();
     Serial.println("Sending data");
     SEND(test1, random(100))
-    SEND(test2, random(10) + 3.14)
     SEND(test3, random(2) == 1? "abc" : "def")
     SEND(test4, 0.01 * random(-100, 100))
     SEND(test5, data)
     SEND(test6, sin((float)millis() / 1000))
-    SEND(test7, i)
     SEND(test8, j)
     Serial.println(millis());
   
-    i += 0.01;
     j += 0.01 * random(-100, 101);
+  }
+  
+  if (millis() - last_sent1 > 1000) {
+    last_sent1 = millis();
+    SEND(test2, random(10) + 3.14)
+    SEND(test7, i)
+    i += 0.01;
   }
 
   if (Serial.available()) {
