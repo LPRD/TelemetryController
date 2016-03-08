3948830f6ddabf00feba11b534c5c37ae1259431
Lucas Kramer <krame505@umn.edu>
2016-03-07 20:47:07 -0600

    Added non-plotted fields for acceleration

--- basic_gui.py
+++ basic_gui.py
@@ -6,8 +6,12 @@ import manager
 import gui
 
 if __name__ == '__main__':
-    dt1 = manager.DataType('force', float, units='Newtons')
-    dispatcher = manager.Dispatcher(dt1)
+    dts = [manager.DataType('force', float, units='Newtons'),
+           manager.DataType('temperature', float, units='deg C'),
+           manager.DataType('x', float, units='Gs', plot=False),
+           manager.DataType('y', float, units='Gs', plot=False),
+           manager.DataType('z', float, units='Gs', plot=False)]
+    dispatcher = manager.Dispatcher(*dts)
     manager = manager.DataManager(dispatcher)
     root = Tk()
     app = gui.Application(dispatcher, manager, master=root)
