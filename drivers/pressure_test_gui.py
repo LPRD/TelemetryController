0719a695260895228542684ac669d589844f7544
Lucas Kramer <krame505@umn.edu>
2017-03-31 13:55:05 -0500

    Modify pressure test gui to log both angles

--- drivers/pressure_test_gui.py
+++ drivers/pressure_test_gui.py
@@ -12,9 +12,10 @@ import plot
 dts = [manager.DataType('pressure_in', float, units='psi', export_csv=True),
        manager.DataType('pressure_out', float, units='psi', export_csv=True),
        manager.DataType('pressure_drop', float, units='psi', export_csv=True),
-       manager.DataType('angle', int, units='degrees', export_csv=True)]
+       manager.DataType('fuel_angle', int, units='degrees', export_csv=True),
+       manager.DataType('oxygen_angle', int, units='degrees', export_csv=True)]
 plots = [plot.Plot('time', ['pressure_in', 'pressure_out', 'pressure_drop']),
-         plot.Plot('time', 'angle')]
+         plot.Plot('time', ['fuel_angle', 'oxygen_angle'])]
          #plot.Plot('pressure', 'angle')]
 dispatcher = manager.Dispatcher(*dts)
 manager = manager.DataManager(dispatcher)
